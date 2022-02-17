4620b797e767cdee6e273be6739a0a41a1f23370
Danabekov <temirrrlan@gmail.com>
2022-02-17 20:37:38 +0600

    Paused on chapter 2 video 11

--- src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -3,7 +3,16 @@ package com.healthycoderapp;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BMICalculatorTest {
 
@@ -47,5 +56,54 @@ class BMICalculatorTest {
 
         // then (assert)
         assertThrows(ArithmeticException.class, executable);
+    }
+
+    @Test
+    void should_ReturnCoderWithWorstBMI_When_CoderListNotEmpty() {
+
+        // given
+        List<Coder> coders = new ArrayList<>();
+        coders.add(new Coder(1.80, 60.0));
+        coders.add(new Coder(1.82, 98.0));
+        coders.add(new Coder(1.82, 64.7));
+
+        // when
+        Coder coderWorstBMI = BMICalculator.findCoderWithWorstBMI(coders);
+
+        // then
+        assertAll(
+                () -> assertEquals(1.82, coderWorstBMI.getHeight()),
+                () -> assertEquals(98.0, coderWorstBMI.getWeight())
+        );
+    }
+
+    @Test
+    void should_ReturnNullWorstBMICoder_When_CoderListEmpty() {
+
+        // given
+        List<Coder> coders = new ArrayList<>();
+
+        // when
+        Coder coderWorstBMI = BMICalculator.findCoderWithWorstBMI(coders);
+
+        // then
+        assertNull(coderWorstBMI);
+    }
+
+    @Test
+    void should_ReturnCorrectBMIScoreArray_When_CoderListNotEmpty() {
+
+        // given
+        List<Coder> coders = new ArrayList<>();
+        coders.add(new Coder(1.80, 60.0));
+        coders.add(new Coder(1.82, 98.0));
+        coders.add(new Coder(1.82, 64.7));
+        double[] expected = {18.52, 29.59, 19.53};
+
+        // when
+        double[] bmiScores = BMICalculator.getBMIScores(coders);
+
+        // then
+        assertArrayEquals(expected, bmiScores);
     }
 }
---
+++ src/test/java/com/healthycoderapp/DietPlannerTest.java
@@ -0,0 +1,25 @@
+package com.healthycoderapp;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DietPlannerTest {
+
+    private DietPlanner dietPlanner;
+
+    @BeforeEach
+    void setup() {
+        this.dietPlanner = new DietPlanner(20, 30, 50);
+    }
+
+    @Test
+    void should_ReturnCorrectDietPlan_When_CorrectCoder() {
+
+        // given (arrange)
+
+
+        // when (act)
+
+        // then (assert)
+    }
+}
