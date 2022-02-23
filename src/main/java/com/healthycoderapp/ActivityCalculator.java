38d6f9e0516f801c968f50ed354a3996d9a0a9e8
tdoff <Nalr1meT190>
2022-02-23 21:46:25 +0600

    tdd approach finished

--- src/main/java/com/healthycoderapp/ActivityCalculator.java
+++ src/main/java/com/healthycoderapp/ActivityCalculator.java
@@ -2,7 +2,20 @@ package com.healthycoderapp;
 
 public class ActivityCalculator {
 
+    private static final int WORKOUT_DURATION_MINS = 45;
+
     public static String rateActivityLevel(int weeklyCardioMin, int weeklyWorkoutSessions) {
-        return "";
+        if (weeklyCardioMin < 0 || weeklyWorkoutSessions < 0) {
+            throw new RuntimeException("Input below 0");
+        }
+
+        int totalMinutes = weeklyCardioMin + weeklyWorkoutSessions * WORKOUT_DURATION_MINS;
+        double avgDailyActivityMins = totalMinutes / 7;
+        if (avgDailyActivityMins < 20) {
+            return "bad";
+        } else if (avgDailyActivityMins < 40) {
+            return "average";
+        }
+        return "good";
     }
 }
